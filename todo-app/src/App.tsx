b6504e454be7d3fb425dff699dbd56d04229330d
kai <kraku47@gmail.com>
2023-12-04 19:02:34 +0000

    Added the array, and started to add the first todo container

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -3,24 +3,27 @@ import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const todos = [
+    { taskUID: '1', priority: 'High', status: 'InProgress', description: 'Complete the todo app', completed: false },
+    { taskUID: '2', priority: 'Medium', status: 'Completed', description: 'Another task or something', completed: true },
+    { taskUID: '3', priority: 'Low', status: 'Normal', description: 'A third task or something', completed: false },
+  ];
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+
+<div>
+          <h3>{todos.description}</h3>
+          <p>Priority: {todos.priority}</p>
+          <p>Status: {todos.status}</p>
+          <p>Completed 
+        </div>
+      ))}
     </div>
   );
 }
 
 export default App;
+
+// For each element of the array, container that has the the UID, priority, status, description 
+// forEach
