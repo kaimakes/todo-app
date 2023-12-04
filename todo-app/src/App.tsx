16c817489e8ad7fc31e4aa0335d1b904429492bb
Lejdi Prifti <lejdi.prifti@linksmt.it>
2023-12-04 20:25:06 +0100

    displayed the todos in dom

--- todo-app/src/App.tsx
+++ todo-app/src/App.tsx
@@ -9,21 +9,44 @@ function App() {
     { taskUID: '3', priority: 'Low', status: 'Normal', description: 'A third task or something', completed: false },
   ];
 
+  // find a way to go from todos that contains JSON elements -> an array that contains JSX elements
 
-  return (
+  // anonymous function
+  const todoElements = todos.map(function (todo) {
+    return (<div>
+      <h3>{todo.description}</h3>
+      <p>Priority: {todo.priority}</p>
+      <p>Status: {todo.status}</p>
+      <p>Completed</p>
+    </div>);
+  });
 
-<div>
-          <h3>{todos.description}</h3>
-          <p>Priority: {todos.priority}</p>
-          <p>Status: {todos.status}</p>
-          <p>Completed 
-        </div>
-      ))}
-    </div>
+  // arrow function
+  const todoElementsMap = todos.map(todo => {
+    // map the JSON todo to a JSX element
+    return (<div>
+      <h3>{todo.description}</h3>
+      <p>Priority: {todo.priority}</p>
+      <p>Status: {todo.status}</p>
+      <p>Completed</p>
+    </div>);
+  });
+
+  // each component in React needs a root/parent element
+  return (<div>
+    {todoElementsMap}
+  </div>
   );
 }
 
 export default App;
 
-// For each element of the array, container that has the the UID, priority, status, description 
+// For each element of the array, display a container that has the the UID, priority, status, description 
+// ----> we will loop inside the array
+
+
+// Looping
+// forEach -> goes through the array and perform actions -> void
+// map -> goes through the array, but expects to return something -> (object, string, etc.) -> used to modify the content of the array
+
 // forEach
